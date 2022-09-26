ec401bb225b875368660fbef5ea21726875aa7d0
9names <60134748+9names@users.noreply.github.com>
2022-09-26 21:13:29 +1000

    Move blinky to another pin to free up JTAG

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -13,16 +13,16 @@ fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
     let parts = dp.GLB.split();
 
-    let mut gpio2 = parts.pin2.into_pull_up_output();
+    let mut gpio17 = parts.pin17.into_pull_up_output();
 
     // Create a blocking delay function based on the current cpu frequency
     let mut d = McycleDelay::new(bl702_hal::SYSFREQ);
 
     loop {
-        gpio2.set_high().unwrap();
+        gpio17.set_high().unwrap();
         d.delay_ms(1000).unwrap();
 
-        gpio2.set_low().unwrap();
+        gpio17.set_low().unwrap();
         d.delay_ms(1000).unwrap();
     }
 }
