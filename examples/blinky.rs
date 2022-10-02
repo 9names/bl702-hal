2d2311247f5fb1e944beea8f3cbdd7ee4c338f48
9names <60134748+9names@users.noreply.github.com>
2022-10-02 13:41:19 +1100

    Use clock config in blinky example

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -3,22 +3,31 @@
 
 use bl702_hal as hal;
 use embedded_hal::digital::blocking::OutputPin;
-use hal::{delay::McycleDelay, pac, prelude::*, clock::{board_clock_init, system_init}};
+use hal::{
+    clock::{board_clock_init, system_init, ClockConfig},
+    delay::McycleDelay,
+    pac,
+    prelude::*,
+};
 use panic_halt as _;
 
 use embedded_hal::delay::blocking::DelayMs;
 
 #[riscv_rt::entry]
 fn main() -> ! {
+    // This *MUST* be called first
     system_init();
+    // Set up default board clock config
     board_clock_init();
     let dp = pac::Peripherals::take().unwrap();
-    let parts = dp.GLB.split();
+    let mut parts = dp.GLB.split();
+    let clocks = ClockConfig::new()
+        .freeze(&mut parts.clk_cfg);
 
     let mut gpio17 = parts.pin17.into_pull_up_output();
 
     // Create a blocking delay function based on the current cpu frequency
-    let mut d = McycleDelay::new(bl702_hal::clock::system_frequency());
+    let mut d = McycleDelay::new(clocks.sysclk().0);
 
     loop {
         gpio17.set_high().unwrap();
