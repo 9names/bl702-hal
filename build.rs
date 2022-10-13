d7d927c264ed4aca81e25fd89a19e15b5ce115ca
9names <60134748+9names@users.noreply.github.com>
2022-10-13 23:46:08 +1100

    Add trap handler from bl602-hal (#5)

--- build.rs
+++ build.rs
@@ -22,6 +22,8 @@ fn main() {
             out_dir.join(format!("lib{}.a", name)),
         )
         .unwrap();
+        // link our trap handler
+        println!("cargo:rustc-link-lib=static={}", name);
 
         println!("cargo:rustc-link-search={}", out_dir.display());
 
